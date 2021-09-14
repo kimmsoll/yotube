85da32f9a35caaa0a136f7ea3e6e37ac53be4349
kimmsoll <kimmsoll@naver.com>
2021-09-14 19:44:23 +0900

    Search 기능 구현
    search 기능 구현

--- src/app.jsx
+++ src/app.jsx
@@ -4,15 +4,28 @@ import './app.css';
 import Nav from "./components/navbar/nav";
 import VideoList from "./components/videoList/video_list";
 
-function App() {
+function App(){
   const [ videos, setVideos ] = useState([]);
-  
+
+  const search = query => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${process.env.REACT_APP_DATA_API}`, requestOptions)
+      .then(response => response.json())
+      // map 안에서 obj 를 ()로 묶어주니 에러가 사라짐
+      .then(result => result.items.map(item => ({...item, id:item.id.videoId})))
+      .then(items => setVideos(items))
+      .catch(error => console.log('error', error));
+  }
+
   useEffect(() => {
     const requestOptions = {
       method: 'GET',
       redirect: 'follow'
     };
-    fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${process.env.REACT_APP_TRENDING_API}`, requestOptions)
+    fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${process.env.REACT_APP_DATA_API}`, requestOptions)
     .then(response => response.json())
     .then(result => setVideos(result.items))
     .catch(error => console.log('error', error));
@@ -20,7 +33,7 @@ function App() {
 
   return (
     <>
-    <Nav></Nav>
+    <Nav onSearch={search}></Nav>
     <VideoList videos={videos}></VideoList>
     </>
   );
--- src/components/navbar/nav.jsx
+++ src/components/navbar/nav.jsx
@@ -1,24 +1,41 @@
-import { Component } from 'react';
+import { useRef } from "react";
 import styles from "./nav.module.css";
 
-class Nav extends Component {
-    render() {
-        return (
-            <>
-            <header className={styles.navbar}>
-                <div className={styles.left}>
-                    <i id={styles.bars} className="fas fa-bars"></i>
-                    <i id={styles.youtube} className="fab fa-youtube"></i>
-                </div>
-                <div className={styles.right}>
-                    <i id={styles.search} className="fas fa-search"></i>
-                    <button>Join</button>
-                    <button>Login</button>
-                </div>
-            </header>
-            </>
-        );
+const Nav = ({onSearch}) => {
+    const inputRef = useRef();
+
+    const handleSearch = () => {
+        const query = inputRef.current.value;
+        onSearch(query);
     }
+
+    const onClick = () => {
+        handleSearch();
+    }
+
+    const onKeypress = (event) => {
+        if(event.key === "Enter"){
+            handleSearch();
+        }
+    }
+    return (
+        <>
+        <header className={styles.navbar}>
+            <div className={styles.left}>
+                <i id={styles.bars} className="fas fa-bars"></i>
+                <i id={styles.youtube} className="fab fa-youtube"></i>
+            </div>
+            <div className={styles.right}>
+                <input ref={inputRef} type="search" name="q" maxLength="30" onKeyPress={onKeypress}/>
+                <button id={styles.search} onClick={onClick}>
+                    <i className="fas fa-search"></i>
+                </button>
+                <button className={styles.join}>Join</button>
+                <button className={styles.login}>Login</button>
+            </div>
+        </header>
+        </>
+    );
 }
 
 export default Nav;
